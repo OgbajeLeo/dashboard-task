9ead3019e956938980208f82595471d5ac95e679
Ogbajeleo <leonardaromeh@gmail.com>
2025-01-18 16:49:01 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-// import { Figtree} from "next/font/google";
-import "./main.css"
+import { Figtree} from "next/font/google";
 import "./globals.css";
+import "./main.css";
 
 
-// const figTree = Figtree({
-//   variable: "--font-fig-tree",
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500", "600", "700", "800", "900"],
-//   fallback:['sans-serif']
-// });
+const figTree = Figtree({
+  variable: "--font-fig-tree",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 
 
 
@@ -25,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` `}>
+      <body className={`${figTree.variable} `}>
         {children}
       </body>
     </html>
