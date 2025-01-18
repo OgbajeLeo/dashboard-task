f5a3750be8912cfe20ec89893f6f641a03cd5dfc
Ogbajeleo <leonardaromeh@gmail.com>
2025-01-18 16:44:25 +0100

    Update on figTree font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Figtree} from "next/font/google";
-import "./globals.css";
 import "./main.css"
+import "./globals.css";
 
 
 // const figTree = Figtree({
