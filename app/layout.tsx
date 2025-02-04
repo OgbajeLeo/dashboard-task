a580892836d186ab21e66b62cff3aef126e5f521
Ogbajeleo <leonardaromeh@gmail.com>
2025-02-04 12:26:37 +0100

    Disabled mobile device access

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${figTree.variable} `}>
+      <body className={`${figTree.variable}`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,17 @@ import Dashboard from "./dashboard/page";
 export default function Home() {
   return (
     <div className="w-full">
+    <div className="w-full lg:block hidden">
       <Sidebar>
         <Dashboard />
       </Sidebar>
+    </div>
+      <div className="w-full lg:hidden flex h-screen justify-center items-center">
+        {/* provide a text to inform the user that this site is only accessible on desktop devices only */}
+        <p className="text-xl text-gray-500 mx-4 text-center">This site is only accessible on
+          desktop devices only</p>
+
+    </div>
     </div>
   );
 }
